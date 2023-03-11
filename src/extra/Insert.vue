e7115bc57772ae855e49a23c992a075ae80df92c
Erfan Rahbari <erfan.rahbari123@gmail.com>
2023-03-11 19:09:42 +0330

    initial commit

---
+++ src/extra/Insert.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button class="btn p-0 bg-light" @click="addImage" data-toggle="tooltip" title="" data-original-title="تصویر">
+          <v-icon>mdi-multimedia</v-icon>
+        </button>
+
+        <button class="btn p-0 bg-light" @click="addLink" data-toggle="tooltip" title="" data-original-title="لینک">
+          <v-icon>mdi-link</v-icon>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Insert",
+    methods: {
+        addImage() {
+            this.$emit('command', 'insertImage', prompt('آدرس عکس خود را وارد فرمایید'));
+        },
+        addLink() {
+            this.$emit('command', 'createLink', prompt('لینک موردنظر خود را وارد فرمایید'));
+        }
+    }
+}
+</script>
+
+<style scoped src="bootstrap/dist/css/bootstrap.css"></style>
+
+<style scoped>
+
+</style>
